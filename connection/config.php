caea5766ec0b32bcc1943afea8fbeb95ff949fb1
Arzl James <108104435+alyb-zcmc@users.noreply.github.com>
2022-09-01 16:05:14 +0800

    YANA

--- connection/config.php
+++ connection/config.php
@@ -3,7 +3,7 @@
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Headers: *");
 
-    $db = mysqli_connect("localhost", "root", "", "zcmc_referral");
+    $db = mysqli_connect("localhost", "root", "","zcmc_referral");
 
     if($db === false){
         die("ERROR: Could not connect. " . mysqli_connect_error());
