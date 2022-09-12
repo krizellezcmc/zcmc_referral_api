06d34b8bbedccca6f5f7ff939575cb0134dd1c6d
krizellezcmc <kmfalcasantos.zcmc@gmail.com>
2022-09-12 10:06:52 +0800

    krzielle

--- api/add_tagubilin.php
+++ api/add_tagubilin.php
@@ -1,14 +1,17 @@
 <?php
  
 include '../connection/config.php';
+include '../functions/tagubilin.php';
+
 
 $method = $_SERVER['REQUEST_METHOD'];
 
- switch($method) {
+switch($method) {
     case 'POST':
 
         $details = json_decode(file_get_contents('php://input'));
 
+        $patRegister = $details->patRegister;
         $patientName = $details->patientName;
         $age = $details->age;
         $sex = $details->sex;
@@ -29,16 +32,58 @@ $method = $_SERVER['REQUEST_METHOD'];
         $followUp = $details->followUp;
         $needBring = $details->needBring;
         $time = $details->time;
-        $healthOthers = $details->healthOthers;
+        $health =  $details->diet;
         $medications = $details->medications;
         $diet = $details->diet;
         $othersDiet = $details->othersDiet;
-        $instructions = $details->instructions;
+        $instructions =  $details->instructions;
         $breastfeed = $details->breastfeed;
         $ob = $details->ob;
-        
 
-    echo json_encode($medications);
+        $medId = uniqid($patRegister);
+        $bfeedId = uniqid($patRegister);
+        $followupId = uniqid($patRegister);
+        $resultId = uniqid($patRegister);
+
+        add_followup($followupId, $followUp, $time, $needBring, $nurse, $resident);
+        add_result($resultId, $laboratory, $xray, $ctScan, $mri, $others);
+     
+        foreach($medications as $key => $value) {
+
+            $medicine = $value->medicine;
+            $dosage = $value->dosage;
+            $sched = $value->sched;
+            $qty = $value->quantity;
+
+            add_medication($medId, $medicine, $dosage, $sched, $qty);
+        }
+
+        foreach($breastfeed as $key => $value) {
+            
+            $date = $value->date;
+            $fromTo = $value->fromTo;
+            $yes = $value->yes;
+            $reason = $value->reason;
+            $management = $value->management;
+            $attended = $value->attended;
+
+            add_breastfeed($bfeedId, $date, $fromTo, $yes, $reason, $management, $attended);
+        }
+
+
+        $stmt = $db->prepare("INSERT INTO tagubilin (PK_tagubilinId, patientName, age, sex, `address`, ward, hrn, admissionDate, dischDate, disch_diagnosis, operation, surgeon, operationDate, FK_resultId, FK_medicationId, FK_breastfeedId, health, instructions, FK_followId) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        
+        $stmt->bind_param("isssssissssssiiissi", $patRegister, $patientName, $age, $sex, $address, $ward, $hrn, $admissionDate, $dischDate, $dischDiag, $operation, $surgeon, $operationDate, $resultId, $medId, $bfeedId, $health, $instructions, $followupId);
+        
+        
+        if($stmt->execute()) {
+            $data = ['status' => 1, 'message' => "success"];
+        } else {
+            $data = ['status' => 0, 'message' => "failed"];
+        }
+
+
+    echo json_encode($data);
     break; 
 }
 
