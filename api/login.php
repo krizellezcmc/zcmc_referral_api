04d39057320e5035d01ce3d3818cd4a8c736b47b
krizellezcmc <kmfalcasantos.zcmc@gmail.com>
2022-09-12 13:58:23 +0800

    Krizelle

--- api/login.php
+++ api/login.php
@@ -18,15 +18,40 @@ $method = $_SERVER['REQUEST_METHOD'];
             if(mysqli_num_rows($result) > 0) {
                 // $data = ['status' => 1, 'message' => "Record successfully created"];
                 while ($user = mysqli_fetch_assoc($result)) {
-                    if(password_verify( $password, $user['password'])) {
+                    if(password_verify($password, $user['password'])) {
                         // IF TAMA PASSWORD
                         if($user['validated'] === 1){
+
+                            $stmt = $db->prepare("UPDATE users SET validated = 1, attempt = 0 WHERE email = ?;");
+                            $stmt->bind_param("s", $email);
+                            $stmt->execute();
                             $data = ['status' => 1, 'loggedIn' => true, "user" => $user];
-                        } else {
+
+                        } else if($user['validated'] === 0) {
                             $data = ['status' => 3, 'message' => "Request pending"];
+                        } else {
+                            $data = ['status' => 5, 'message' => "User blocked!"];
                         }
+
                     }else {
-                        $data = ['status' => 0, 'message' => "Invalid password"];
+
+                        if($user['attempt'] === 3) {    
+                            
+                            $data = ['status' => 5, 'message' => "User blocked!"];
+
+                            $stmt = $db->prepare("UPDATE users SET validated = 3 WHERE email = ?;");
+                            $stmt->bind_param("s", $email);
+                            $stmt->execute();
+
+                        } else {
+
+                            $stmt = $db->prepare("UPDATE users SET attempt = attempt + 1 WHERE email = ?;");
+                            $stmt->bind_param("s", $email);
+                            $stmt->execute();
+
+                            $data = ['status' => 0, 'message' => "Invalid password", 'logs' => $user['attempt']];
+                        }
+                  
                     }
                 }
             } else {
