59449766f4050dd0970abe3da5419a3936e50e65
Krizelle Mae Falcasantos - ZCMC <108104159+krizellezcmc@users.noreply.github.com>
2022-09-17 08:45:54 +0800

    Update config.php

--- connection/config.php
+++ connection/config.php
@@ -1,15 +1,4 @@
-<!-- <?php
 
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: *");
-
-    $db = mysqli_connect("localhost", "root", "","zcmc_referral");
-
-    if($db === false){
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-    } -->
-        
-        
 <?php
 
     session_start();
